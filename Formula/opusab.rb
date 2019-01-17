86397fbeffa2ef23a8b18347f45ccd40d7d9c786
Nikita Starshinov <nstarshinov@yandex.ru>
2019-01-17 17:53:48 +0300

    Update opusab: add revision

--- Formula/opusab.rb
+++ Formula/opusab.rb
@@ -1,7 +1,7 @@
 class Opusab < Formula
   desc "Create Opus audiobooks from a list of mp3 files"
   homepage "https://github.com/nikstar/opusab"
-  url "https://github.com/nikstar/opusab.git", :tag => "v1.0-beta"
+  url "https://github.com/nikstar/opusab.git", :tag => "v1.0-beta", :revision => "3a890eecad9c4c16c826b69c2594fd47ed61341a"
   head "https://github.com/nikstar/opusab.git"
 
   depends_on :xcode => ["10.0", :build]
