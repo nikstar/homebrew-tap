ba0385dc81e614f3bce1bfd69086a847fd377d4c
Nikita Starshinov <nstarshinov@yandex.ru>
2019-01-17 17:37:43 +0300

    Update opusab formula

--- Formula/opusab.rb
+++ Formula/opusab.rb
@@ -1,18 +1,19 @@
 class Opusab < Formula
-  desc "Is it Opusab or opusab?"
-  homepage "https://github.com/nikstar/Opusab"
-  url "https://github.com/nikstar/Opusab.git"
-  version "0.1"
+  desc "Create Opus audiobooks from a list of mp3 files"
+  homepage "https://github.com/nikstar/opusab"
+  url "https://github.com/nikstar/opusab.git", :tag => "v1.0-beta"
+  head "https://github.com/nikstar/opusab.git"
 
   depends_on :xcode => ["10.0", :build]
   depends_on "ffmpeg"
   depends_on "media-info"
 
   def install
-    system "make", "install", "prefix=#{prefix}"
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/opusab"
   end
 
   test do
-    system "true"
+    system "#{bin}/opusab", "--help"
   end
 end
